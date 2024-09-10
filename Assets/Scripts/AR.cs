ababd13e27255e2fbbe4c7fc84651123b809409b
George Jimy <103298857+GeorgeHuaman@users.noreply.github.com>
2024-09-10 18:18:07 -0500

    Upload Project

---
+++ Assets/Scripts/AR.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zappar;
+
+public class AR : MonoBehaviour
+{
+    public ZapparInstantTrackingTarget instantTrackingTarget;
+    private bool hasPlacedModel = false; // Variable para controlar que solo se coloque una vez
+
+    void Update()
+    {
+        // Si no hemos colocado el modelo y se detecta un toque en la pantalla
+        if (!hasPlacedModel && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            // Llamar a PlaceInModel solo una vez
+            PlaceModel();
+        }
+    }
+
+    void PlaceModel()
+    {
+        // Desactivar el seguimiento una vez que se ha colocado el modelo
+        instantTrackingTarget.PlaceTrackerAnchor();
+
+        // Marcar que el modelo ya fue colocado para no hacerlo de nuevo
+        hasPlacedModel = true;
+    }
+}
